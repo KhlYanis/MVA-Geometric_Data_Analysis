d7d6cd061885e1f02cf84c884b780ff93e676e6b
Yaniiiuss <yanis.kahil01@gmail.com>
2024-11-13 20:32:18 +0100

    Connectiviy computation

--- data/ABIDEProcessing.py
+++ data/ABIDEProcessing.py
@@ -4,9 +4,11 @@ from nilearn import connectome
 import os
 import numpy as np
 from pathlib import Path
+import scipy.io as sio
 
 ## ROOT_FOLDER
 ROOT_FOLDER = Path(__file__).parent.parent.absolute()
+
 ## DATA_FOLDER : Where we can find the imaging dataset
 DATA_FOLDER = os.path.join(ROOT_FOLDER, 'data\\ABIDE_dataset\\ABIDE_pcp\\cpac\\filt_noglobal\\')
 ## Path for the phenotypic dataset (.csv)
@@ -60,3 +62,32 @@ def get_timeseries(subject_IDs, FileNames):
         timeseries.append(np.loadtxt(DATA_FOLDER + filename[0], skiprows=0))
     
     return timeseries
+
+def get_subject_connectivity(time_series, subjectID, kind = 'correlation', save = True):
+    """
+    time_series : timeseries table for a certain subject [timepoints, region]
+    subjectID : the subject ID
+    kind : type of connectivity to be used (In our case, we use the correlation)
+    save : flag to save the connectivity matrix somewhere
+    save_path : path to save the connectivity matrix
+    """
+    # Create a directory to save the connectivity matrices if not already existing
+    save_directory = os.path.join(ROOT_FOLDER, 'data', 'Connectivity_matrices')
+
+    if os.path.exists(save_directory) == False :
+        os.makedirs(save_directory)
+        print("The saving directory for connectivity matrices is created")
+    else : 
+        print("The saving directory for connecitivity matrices already exists")
+
+    # Now, we can compute the connectivity matrix for the subject
+    print(f"Computing the connectivity matrix for subject {subjectID}")
+
+    conn_measure = connectome.ConnectivityMeasure(kind = kind)
+    connectivity_mat = conn_measure.fit_transform([time_series])[0]
+
+    if save :
+        subject_file = os.path.join(save_directory, subjectID + '_' + kind + '_' + '.mat' )
+        sio.savemat(subject_file, {'connectivity' : connectivity_mat})
+
+    return connectivity_mat
